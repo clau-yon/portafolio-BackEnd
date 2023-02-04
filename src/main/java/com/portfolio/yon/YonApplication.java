fd1f5ff475b094e06f5d3291ab6cce500945c5ec
Claudia Yon <clau.yon.15@mi.unc.edu.ar>
2023-02-04 09:54:36 -0300

    .

---
+++ src/main/java/com/portfolio/yon/YonApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.yon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/yon/YonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.yon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
